167b7fc892e696c984af4c75d2a0c988b7563e58
yonseung kim <ykim@yonseungs-MacBook-Pro.local>
2017-07-04 14:03:02 -0400

    Exercise 6: One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Yonseung", last_name: "Kim", hourly_rate: 40)
+
+@store2 = Store.find(2)
+@store2.employees.create(first_name: "Dennis", last_name: "Lee", hourly_rate: 40)
+@store2.employees.create(first_name: "Kally", last_name: "Park", hourly_rate: 70)
